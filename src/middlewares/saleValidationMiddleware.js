b3c404c33ab5c85fa63ece0bf78abcf44ce18900
Jonas Tiago <jonastiago95@gmail.com>
2022-11-28 16:00:01 -0300

    refactor: route sale

--- src/middlewares/saleValidationMiddleware.js
+++ src/middlewares/saleValidationMiddleware.js
@@ -3,20 +3,19 @@ import { productCollection } from "../database/db.js";
 
 export async function saleValidation(req, res, next) {
   const { products, priceTotal } = req.body;
-  const userId = /*res.locals.user_id*/ 123456;
+  const userId = res.locals.user_id;
 
   products.forEach(async (product) => {
     try {
       const productAvailable = await productCollection.findOne({
-        _id: ObjectId(product.id),
+        _id: ObjectId(product._id),
       });
-      console.log(parseInt(product.quantity));
-      console.log(parseInt(productAvailable.quantity));
 
       if (!productAvailable) return res.sendStatus(404);
 
-      if (parseInt(product.quantity) > parseInt(productAvailable.quantity))
+      if (parseInt(product.quantity) > parseInt(productAvailable.quantity)) {
         return res.sendStatus(404);
+      }
     } catch (err) {
       console.log(err);
       res.sendStatus(500);
@@ -25,6 +24,7 @@ export async function saleValidation(req, res, next) {
   const sale = {
     products,
     priceTotal,
+    payment,
     userId,
   };
 
