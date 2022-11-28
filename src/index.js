7db2f586d4e908037fa792e3ac274d77d7c11b72
luskamusca <luskamuskammouse@gmail.com>
2022-11-28 01:43:50 -0300

    feat: POST signup and signin

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import authRouters from "./routes/authRoutes.js"
 import productsRouter from "./routes/productsRoutes.js";
 dotenv.config();
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(authRouters);
 app.use(productsRouter);
 
 const port = process.env.PORT || 5000;
