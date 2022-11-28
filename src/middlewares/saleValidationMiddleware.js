b617363d690e1e814b2663f251068e2e07097653
Jonas Tiago <jonastiago95@gmail.com>
2022-11-28 09:17:17 -0300

    feat: meddleware sale validat

--- src/middlewares/saleValidationMiddleware.js
+++ src/middlewares/saleValidationMiddleware.js
@@ -1,13 +1,39 @@
+import { productCollection } from "../database/db";
+
 export async function saleValidation(req, res, next) {
   const { products, priceTotal } = req.body;
   const user_id = res.locals.user_id;
 
-  const sale = {
-    products: {
-      id,
-      quantity,
-    },
-    priceTotal,
-    user_id,
-  };
+  products.forEach( async (product) => {
+    try {
+        const productAvailable = await productCollection.find({id:product.id}).toArray()
+        
+        if(!productAvailable) return res.sendStatus(404)
+
+        if(product.quantity > productAvailable.quantity) return res.sendStatus(404)
+
+        const sale = {
+            products: [
+              {
+                id,
+                quantity,
+              },
+              {
+                id,
+                quantity,
+              },
+            ],
+            priceTotal,
+            user_id,
+          };
+
+          res.locals.sale = sale
+
+    } catch (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+  });
+
+  next()
 }
